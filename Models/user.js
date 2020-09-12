52b37712287219355e23b5efd3c3f56edf310ab5
kikusourabh <karmakarsourabh7@gmail.com>
2020-09-12 17:39:15 +0530

    registration-done

--- Models/user.js
+++ Models/user.js
@@ -1,1 +1,13 @@
 const mongoose = require("mongoose");
+
+const user = new mongoose.Schema({
+  uname: String,
+  fullname: String,
+  profile: String,
+  dateofbirth: String,
+  bio: String,
+  email: String,
+  password: String,
+});
+
+module.exports = mongoose.model("user", user);
--- router.js
+++ router.js
@@ -1,7 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const user = require("./Models/user");
 
 router.get("/", (req, res) => {
   res.send("Wellcome to instagram rest api");
+});
+
+router.post("/register_user", async (req, res) => {
+  try {
+    const { uname, email, password } = req.body;
+    if (!uname || !email || !password) {
+      res.send({
+        error: "bad request",
+      });
+    } else {
+      const userExist = await user.findOne({ uname: uname, email: email });
+      if (userExist) {
+        res.send({
+          error: "user already exist",
+        });
+      } else {
+        const User = new user({
+          uname: uname,
+          email: email,
+          password: password,
+        });
+        const savedUser = await User.save();
+        res.send({
+          msg: "user register successfully",
+          user: savedUser,
+        });
+      }
+    }
+  } catch (error) {
+    res.send({
+      error: "some thing went wrong",
+    });
+  }
 });
 module.exports = router;
